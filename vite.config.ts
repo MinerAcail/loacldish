6c7d0d32d69e9499d7135ffc6b9387ecc28d5819
miner acail <mineracail@gmail.com>
2025-03-27 10:29:06 +0000

    fix json

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: '0.0.0.0', // Allows external access (useful for EC2)
-    port: 4000, // Custom port for dev
-  },
-  build: {
-    outDir: 'dist', // Ensure correct output directory for PM2
-  },
-  base: '/', // Set correct base path (important if using Caddy reverse proxy)
-});
+  preview: {
+    allowedHosts: ['localdish.winnemart.com'], // 👈 Add this line
+    port: 3456, // Change the preview port if needed
+  }
+})
